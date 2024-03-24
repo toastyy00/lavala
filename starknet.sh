28b79101a1aad2a9126e1c120f15a4c86ddb0562
toasty <mhmdarf1818@gmail.com>
2024-03-24 19:51:58 +0700

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-9f314077-5b58-4e7e-9f0c-4e6dff7a018f/"
 
 OUTPUT_DIR="logs"
 
